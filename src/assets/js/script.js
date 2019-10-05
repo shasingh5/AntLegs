2aee0c52751b67a1cdb60c0f36f83926091dbc8d
shasingh5 <shashi4singh@gmail.com>
2019-10-05 16:09:19 +0530

    registration page added.

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -193,7 +193,39 @@
         $(".forgot-block").slideToggle();
     });
 
-    
+    $('.testimonials-slider').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,                
+              }
+            },
+            {
+              breakpoint: 815,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+    });    
 
 })(jQuery);
 
