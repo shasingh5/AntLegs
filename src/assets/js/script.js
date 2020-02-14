66d786348b26e44ffd378da5d92120e798d722f4
shasingh5 <shashi4singh@gmail.com>
2020-02-14 11:16:54 +0530

    cv-display-recruiter-2 revised html of cv-display-recruiter.

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -273,6 +273,38 @@
         ]
     });
 
+    //recommended job card slider
+    $('.rj-card-slider').slick({
+        infinite: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: true,
+        responsive: [{
+            breakpoint: 920,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+            }
+        }, {
+            breakpoint: 769,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        }, {
+            breakpoint: 481,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+
 
 
 
